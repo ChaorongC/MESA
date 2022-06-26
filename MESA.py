39ceeff3b2b0019e8d81e147b471676d9f6385c5
crchen12 <108202347+ChaorongC@users.noreply.github.com>
2022-06-25 18:40:09 -0700

    Update MESA.py

--- MESA.py
+++ MESA.py
@@ -1,5 +1,5 @@
 """
-  @Content: Feature selection code for Stanley
+  @Content: Feature selection code for MESA
   @Author: Chaorong Chen
   @Date: 2022-06-12 00:20:08
   @Last Modified by: Chaorong Chen
@@ -15,16 +15,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC, LinearSVC
 from sklearn.metrics import roc_auc_score
 from boruta import BorutaPy
-
-# Load data sets (cohort 1 as example)
-X_c1_t1 = pd.read_csv("dir_to_data/cohort1_type1.csv",index_col=0)
-X_c1_t2 = pd.read_csv("dir_to_data/cohort1_type2.csv",index_col=0)
-X_c1_t3 = pd.read_csv("dir_to_data/cohort1_type3.csv",index_col=0)
-X_c1_t4 = pd.read_csv("dir_to_data/cohort1_type4.csv",index_col=0)
-X_c1_t5 = pd.read_csv("dir_to_data/cohort1_type5.csv",index_col=0)
-X_c1_t6 = pd.read_csv("dir_to_data/cohort1_type6.csv",index_col=0)
-y_cohort1 = pd.read_csv("dir_to_data/cohort1_label.csv",index_col=0).T.values[0]
-
 
 # Code for sequntial backward selection 
 def SBS_LOO(X,
@@ -222,40 +212,3 @@ def calculate_combine(X, y, feature_selected, classifiers):
     auc = [roc_auc_score(y_true, prob)
            for prob in y_pred_all]  # calculate AUC for all classifiers
     return y_true, y_pred_all, auc
-    
-    
-    
-    
-# Example
-random_state = 0
-
-# Train-test split inside SBS
-cv_sbs = StratifiedKFold(n_splits=5, random_state=random_state, shuffle=True)
-
-# Classifiers used for final evaluation on test sets
-svm = SVC(kernel="linear",random_state=random_state,probability=True)
-rf = RandomForestClassifier(random_state=random_state,n_jobs=-1)
-
-# Run pipeline for cohort 1, type 1 feature
-sbs_c1_t1 = SBS_LOO(X=X_c1_t1,
-             y=y_cohort1,
-             estimator=svm,
-             cv=cv_sbs,
-             classifiers=[svm, rf],
-             min_feature=10,
-             boruta_top_n_feature=100)
-# Run pipeline for cohort 1, type 2 feature
-sbs_c1_t2 = SBS_LOO(X=X_c1_t2,
-             y=y_cohort1,
-             estimator=svm,
-             cv=cv_sbs,
-             classifiers=[svm, rf],
-             min_feature=10,
-             boruta_top_n_feature=100)
-
-#Integrate results(feature selected) of type 1 and type 2 features for cohort 1
-sbs_c1_combine = calculate_combine(
-    X=[X_c1_t1, X_c1_t2],
-    y=y_cohort1,
-    feature_selected=[sbs_c1_t1[0], sbs_c1_t2[0]],
-    classifiers=[svm, rf])
